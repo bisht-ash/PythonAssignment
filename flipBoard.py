4be7540344c95da5765e8f448509fe1b7a62b768
Aswat Singh Bisht <aswat.b@sigmoidanalytics.com>
2023-02-13 18:03:54 +0530

    Exceptional handling

--- flipBoard.py
+++ flipBoard.py
@@ -16,7 +16,13 @@ class Solution:
         if(board==[]):
             raise Exception("Empty Input")
         else:
+            if(len(board)<self.totalRow):
+                raise Exception("Number of row not satisfied")
+            # if less number of rows are provided
             for row in range(self.totalRow):
+                if(len(board[row])<self.totalCol):
+                    raise Exception("Number of column not satisfied")
+                # if less number of cols are provided
                 for col in range(self.totalCol):
                     # checking if there is something else in the board
                     if(board[row][col]!='X' and board[row][col]!='O'):
@@ -25,15 +31,15 @@ class Solution:
 
     # flipBoard function which does the main work 
     def flipBoard(self, board):
-        # dx and dy are directional list 
-        # allows us to travel in all four direction easily 
+        '''dx and dy are directional list 
+        allows us to travel in all four direction easily '''
         dx=[1,0,-1,0]
         dy=[0,1,0,-1]
         # created a queue to perform Breath First Search
         bfsQueue=deque()
 
-        # added all the boundary elements to the queue 
-        # we will perform a multi source BFS
+        '''added all the boundary elements to the queue 
+        we will perform a multi source BFS'''
         for row in range(self.totalRow):
             if(board[row][0]=='O'):
                 bfsQueue.append([row,0])
@@ -46,17 +52,17 @@ class Solution:
             if(board[self.totalRow-1][col]=='O'):
                 bfsQueue.append([self.totalRow-1,col])
 
-        # BFS starts here 
-        # we marked all the Os that are connected to some boundary O
-        # and then we change all the other Os to X.
-        # we did this by marking all the Os that can be visited if we start the BFS from any boundary O
+        '''BFS starts here 
+        we marked all the Os that are connected to some boundary O
+        and then we change all the other Os to X.
+        we did this by marking all the Os that can be visited if we start the BFS from any boundary O'''
         while(len(bfsQueue)):
             # poping the first element 
             x=bfsQueue.popleft()
             if(board[x[0]][x[1]]=='O'):
-                # if the current O is not visited we mark it as visited 
-                # check all its 4 neighbour if they are O and are not visited 
-                # then push them to the queue
+                '''if the current O is not visited we mark it as visited 
+                check all its 4 neighbour if they are O and are not visited 
+                then push them to the queue'''
                 board[x[0]][x[1]]='*'
                 for i in range(4):
                     # this loop is to iterate over all 4 neighbour
@@ -82,11 +88,16 @@ def main():
     # taking input board 
     for i in range(totalRow):
         board.append(input().split(" "))
-        
+    
     # Creating the solution object
     solutionObject= Solution(totalRow,totalCol)
-    solutionObject.flipBoard(board)
-    print(board)
+    try:
+        solutionObject.validateInput(board)
+    except Exception as e:
+        print(str(e))
+    else:
+        solutionObject.flipBoard(board)
+        print(board)
 
 
 
--- test_assignment.py
+++ test_assignment.py
@@ -1,5 +1,5 @@
 import pytest
-from Question1 import Solution
+from flipBoard import Solution
 
 @pytest.fixture
 def object():
@@ -10,19 +10,24 @@ def object():
 def test_empty_list():
     board=[]
     emptyObject=Solution(0,0)
+    # when we get an empty board we should raise an exception
     with pytest.raises(Exception):
         emptyObject.validateInput(board)
     
 def test_allXsInput(object):
+    # if all the values are X
     board=[['X','X','X'],['X','X','X'],['X','X','X']]
     object.validateInput(board)
     object.flipBoard(board)
+    # The ans should remain the same
     assert board==[['X','X','X'],['X','X','X'],['X','X','X']]
 
 def test_allOsInput(object):
+    # if all the values are O
     board=[['O','O','O'],['O','O','O'],['O','O','O']]
     object.validateInput(board)
     object.flipBoard(board)
+    # The ans should remain the same
     assert board==[['O','O','O'],['O','O','O'],['O','O','O']]   
 
 
@@ -34,6 +39,13 @@ def test_normalInput(object):
 
 
 def test_InvalidInput(object):
+    # when we get a board with wrong values we should raise an exception
     board=[['O','*','X'],['X','O','X'],['X','*','X']]
+    with pytest.raises(Exception):
+        object.validateInput(board)
+        
+def test_WrongAmountInput(object):
+    # when we get a board with wrong amount of column we should raise an exception
+    board=[['O','*','X'],['XXX'],['X','*','X']]
     with pytest.raises(Exception):
         object.validateInput(board)
